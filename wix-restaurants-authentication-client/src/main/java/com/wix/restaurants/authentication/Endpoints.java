a5de82d2abd3bc146125c3f23d62e3f4f9925dde
Danny Leshem <dleshem@gmail.com>
2016-10-21 22:59:51 +0300

    Added Endpoints.INTERNAL

--- wix-restaurants-authentication-client/src/main/java/com/wix/restaurants/authentication/Endpoints.java
+++ wix-restaurants-authentication-client/src/main/java/com/wix/restaurants/authentication/Endpoints.java
@@ -1,5 +1,8 @@
 package com.wix.restaurants.authentication;
 
 public class Endpoints {
+	/** Production URL, use this for all purposes. */
 	public static final String PRODUCTION = "https://auth.openrest.com/v1.0";
+    /** Internal URL that may change without notice. Can be tried as fallback during DNS outages. */
+	public static final String INTERNAL = "https://wix-restaurants-authentication.appspot.com/v1.0";
 }
